e2cc4f3c86a2c656969b9722fee1c7f0b38c5900
Rosaldo Garcia <rosaldo.garcia@hcl.com>
2022-08-14 08:44:10 +0800

    *

---
+++ vars/helloWorld.groovy
@@ -0,0 +1,15 @@
+// def call(Map config = [:]) {
+//     sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
+// }
+
+// def call(String name, String dayOfWeek){
+//     sh "echo Hello ${name}. Today is ${dayOfWeek}."
+// }
+
+// def call{
+//     sh "echo Hello World"
+// }
+
+def call(Map config = [:]) {
+    sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
+}
