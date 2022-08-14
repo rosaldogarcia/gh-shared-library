91412a7d85046bc9e13d053fea36b6948a6fb573
Rosaldo Garcia <rosaldo.garcia@hcl.com>
2022-08-14 11:53:21 +0800

    *

--- vars/runws.groovy
+++ vars/runws.groovy
@@ -1,17 +1,4 @@
 def call() {
     loadLinuxScript(name: 'hello-world.sh')
     sh "./hello-world.sh true true"
-// }
-stage('Example') {
-        {
-          int appscanStatus = sh(
-            script: """
-            echo "Making script executable"
-            chmod a+x ../RunAppScanAutomation.sh
-            ../RunAppScanAutomation.sh ${ctx.job} ${ctx.appsid}
-            """,
-            returnStatus: true
-          )
-        }
-   }
 }
