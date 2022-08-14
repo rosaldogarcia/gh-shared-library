16d9a6037a96b019d22bfba2bd3fb6d2991beb37
Rosaldo Garcia <rosaldo.garcia@hcl.com>
2022-08-14 12:00:12 +0800

    *

--- resources/com/planetpope/scripts/linux/hello-world.sh
+++ resources/com/planetpope/scripts/linux/hello-world.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # echo Hello $1. Today is $2.
 
-
 rossjs=$1
-if [ "$rossjs" = true ]; then
+if [ "$rossjs" = rossjs=true ]; then
   echo "process the script for rossjs"
 fi
 
 rossconverters=$2
-if [ "$rossconverters" = true ]; then
+if [ "$rossconverters" = rossconverters=true ]; then
   echo "process the script for rossconverters"
 fi
 
