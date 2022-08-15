af4199b9ae0d58e4429dd49a3e1f18d35d677372
Rosaldo Garcia <rosaldo.garcia@hcl.com>
2022-08-15 10:43:45 +0800

    *

--- vars/log.groovy
+++ vars/log.groovy
@@ -4,6 +4,7 @@ def js(message) {
         data = libraryResource(resource: 'sample.sh', encoding: 'Base64')
         writeFile file: 'sample.sh', text: data, encoding: 'Base64'
     }
+    node('wss') {
     stage('example') {
             int wsstatus = sh(
                 script: """
