1f0f7e87c7471ad13d351533ad5a2a834f8b5686
Rosaldo Garcia <rosaldo.garcia@hcl.com>
2022-08-15 10:22:27 +0800

    *

--- vars/log.groovy
+++ vars/log.groovy
@@ -2,7 +2,7 @@ def js(message) {
     echo "rossjs: ${message}"
     stage('exammple') {
         data = libraryResource(resource: 'sample.sh', encoding: 'Base64')
-        whitefile file: 'sample.sh', text: data, encoding: 'Base64')
+        writefile file: 'sample.sh', text: data, encoding: 'Base64')
     }
     stage('example') {
         {
