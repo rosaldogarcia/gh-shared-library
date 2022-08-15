e49efafc1154d544bcc2a64e8b5c6bbd95f7386c
Rosaldo Garcia <rosaldo.garcia@hcl.com>
2022-08-15 10:26:08 +0800

    *

--- vars/log.groovy
+++ vars/log.groovy
@@ -5,7 +5,6 @@ def js(message) {
         writefile file: 'sample.sh', text: data, encoding: 'Base64'
     }
     stage('example') {
-        {
             int wsstatus = sh(
                 script: """
                 echo "making script executable"
@@ -14,7 +13,6 @@ def js(message) {
                 returnStatus: true
             )
         }
-    }
 }
 
 def converters(message) {
